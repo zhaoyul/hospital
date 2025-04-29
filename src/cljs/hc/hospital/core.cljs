67e5271a64a353a968ce8a349bc891c97608760d
kevin li <12980829@qq.com>
2025-04-29 22:48:15 +0800

    [feat]: 添加 Ant Design UI 库并配置 clj-kondo
    - 添加: Ant Design (`antd`) UI 库到项目依赖.
    - 添加: clj-kondo 配置以支持 `next.jdbc/with-transaction` 解析.
    - 修改: 在 `core.cljs` 中引入并使用 Ant Design 的 `Button` 组件.

--- src/cljs/hc/hospital/core.cljs
+++ src/cljs/hc/hospital/core.cljs
@@ -1,13 +1,19 @@
 (ns hc.hospital.core
     (:require
       [reagent.core :as r]
-      [reagent.dom :as d]))
+      [reagent.dom :as d]
+      ["antd" :as antd :refer [Button]])) ; Import Button component from antd
+
+;; Import Ant Design CSS
+;;(js/require "antd/dist/reset.css")
 
 ;; -------------------------
 ;; Views
 
 (defn home-page []
-  [:div [:h2 "Welcome to Reagent!"]])
+  [:div
+   [:h2 "Welcome to Reagent!"]
+   [:> Button {:type "primary"} "Hello Ant Design!"]]) ; Use the Button component
 
 ;; -------------------------
 ;; Initialize app
