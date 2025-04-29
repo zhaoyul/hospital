e8325a9e347779b7017934d14c4f4d8c83ee94c0
kevin li <12980829@qq.com>
2025-04-30 00:45:31 +0800

    [ref]: 重构麻醉评估页面UI布局
    - 提取 `radio-with-input` 组件以简化包含"无/有"选项和关联输入框的表单项。
    - 重构 "简要病史" 部分的布局, 使用 Flexbox 替代原有的 Grid 布局, 并移除外层 Card。
    - 移除 "体格检查" 和 "实验室检查" 部分的外层 Card 组件。
    - 新增 `assessment-result` 组件, 使用 Ant Design 的 Tabs (标签页) 组件 (左侧垂直布局) 来组织 "简要病史", "体格检查" 和 "实验室检查"。
    - 调整主页面布局, 更新患者列表和评估结果区域的栅格比例 (从 8:16 改为 6:18)。
    - 为患者列表区域添加 Card 包裹。

--- src/cljs/hc/hospital/pages/anesthesia_home.cljs
+++ src/cljs/hc/hospital/pages/anesthesia_home.cljs
@@ -40,140 +40,144 @@
                               "已驳回" "red"
                               "default")} (:status item)]]])])
 
+(defn radio-with-input [label]
+  (let [option (r/atom "none")]  ;; 默认选择"无"
+    (fn []
+      [:div.form-item-inline {:style {:display "flex" :alignItems "center" :marginBottom "16px"}}
+       [:label {:style {:minWidth "100px" :fontWeight "500" :marginRight "10px"}} label]
+       [antd/radio-group {:value @option
+                          :onChange #(reset! option (.. % -target -value))
+                          :style {:marginRight "10px"}}
+        [antd/radio {:value "none"} "无"]
+        [antd/radio {:value "yes"} "有"]]
+       [antd/input {:placeholder "请输入描述内容"
+                    :disabled (= @option "none")
+                    :style {:flex "1"}}]])))
+
 (defn brief-medical-history []
-  [antd/card {:title (r/as-element [antd/title {:level 5} "简要病史"]) :variant "borderless"} ; Use wrapped components
-   [antd/form {:layout "vertical"}
-    [antd/row {:gutter 16}
-     [antd/col {:span 12}
-      [antd/form-item {:label "既往史"}
-       [antd/radio-group {}
-        [antd/radio {:value "none"} "无"]
-        [antd/radio {:value "yes"} "有"]]
-       [antd/input {:placeholder "请输入"}]]]
-     [antd/col {:span 12}
-      [antd/form-item {:label "过敏史"}
-       [antd/radio-group {}
-        [antd/radio {:value "none"} "无"]
-        [antd/radio {:value "yes"} "有"]]
-       [antd/input {:placeholder "请输入"}]]]
-     [antd/col {:span 12}
-      [antd/form-item {:label "手术麻醉史"}
-       [antd/radio-group {}
-        [antd/radio {:value "none"} "无"]
-        [antd/radio {:value "yes"} "有"]]
-       [antd/input {:placeholder "请输入"}]]]
-     [antd/col {:span 12}
-      [antd/form-item {:label "怀孕"}
-       [antd/radio-group {}
-        [antd/radio {:value "none"} "无"]
-        [antd/radio {:value "yes"} "有"]]
-       [antd/input {:placeholder "请输入"}]]]
-     [antd/col {:span 12}
-      [antd/form-item {:label "输血史"}
-       [antd/radio-group {}
-        [antd/radio {:value "none"} "无"]
-        [antd/radio {:value "yes"} "有"]]
-       [antd/input {:placeholder "请输入"}]]]
-     [antd/col {:span 12}
-      [antd/form-item {:label "月经期"}
-       [antd/radio-group {}
-        [antd/radio {:value "none"} "无"]
-        [antd/radio {:value "yes"} "有"]]
-       [antd/input {:placeholder "请输入"}]]]
-     [antd/col {:span 24}
-      [antd/form-item {:label "个人史"}
-       [antd/checkbox-group {}
-        [antd/checkbox {:value "smoke"} "烟"]
-        [antd/checkbox {:value "drink"} "酒"]]]]
-     [antd/col {:span 24}
-      [antd/form-item {:label "其他"}
-       [antd/input {:placeholder "请输入"}]]]]]])
+  [antd/form {:layout "vertical" :style {:padding-bottom "24px"}}
+   [radio-with-input "既往史"]
+   [radio-with-input "过敏史"]
+   [radio-with-input "手术麻醉史"]
+   [radio-with-input "怀孕"]
+   [radio-with-input "输血史"]
+   [radio-with-input "月经期"]
+   [:div {:style {:display "flex" :alignItems "center" :marginBottom "16px"}}
+    [:label {:style {:minWidth "100px" :fontWeight "500" :marginRight "10px"}} "个人史"]
+    [antd/checkbox-group {}
+     [antd/checkbox {:value "smoke"} "烟"]
+     [antd/checkbox {:value "drink"} "酒"]]]
+   [:div {:style {:display "flex" :alignItems "center" :marginBottom "16px"}}
+    [:label {:style {:minWidth "100px" :fontWeight "500" :marginRight "10px"}} "其他"]
+    [antd/input {:placeholder "请输入" :style {:flex "1"}}]]])
 
 (defn physical-examination []
-  [antd/card {:title (r/as-element [antd/title {:level 5} "体格检查"]) :variant "borderless"} ; Use wrapped components
-   [antd/form {:layout "vertical"}
-    [antd/form-item {:label "一般状况"}
-     [antd/radio-group {}
-      [antd/radio {:value "bad"} "差"]
-      [antd/radio {:value "fair"} "尚可"]
-      [antd/radio {:value "average"} "一般"]
-      [antd/radio {:value "good"} "好"]]]
-    [antd/row {:gutter 16}
-     [antd/col {:span 8} [antd/form-item {:label "身高"} [antd/input-number {:addonAfter "cm"}]]]
-     [antd/col {:span 8} [antd/form-item {:label "体重"} [antd/input-number {:addonAfter "kg"}]]]
-     [antd/col {:span 8} [antd/form-item {:label "BP"} [antd/input {:addonAfter "mmHg"}]]]
-     [antd/col {:span 8} [antd/form-item {:label "PR"} [antd/input {:addonAfter "次/分"}]]]
-     [antd/col {:span 8} [antd/form-item {:label "RR"} [antd/input {:addonAfter "次/分"}]]]
-     [antd/col {:span 8} [antd/form-item {:label "T"} [antd/input {:addonAfter "°C"}]]]]
-    [antd/form-item {:label "精神行为"}
-     [antd/radio-group {}
-      [antd/radio {:value "normal"} "正常"]
-      [antd/radio {:value "drowsy"} "嗜睡"]
-      [antd/radio {:value "coma"} "昏迷"]
-      [antd/radio {:value "irritable"} "烦躁"]
-      [antd/radio {:value "delirium"} "谵妄"]
-      [antd/radio {:value "cognitive"} "认知障碍"]]]
-    [antd/form-item {:label "头颈部"}
-     [antd/checkbox-group {}
-      [antd/checkbox {:value "normal"} "无异常"]
-      [antd/checkbox {:value "scar"} "疤痕"]
-      [antd/checkbox {:value "short_neck"} "颈短"]
-      [antd/checkbox {:value "neck_mass"} "颈部肿块"]
-      [antd/checkbox {:value "limited_mobility"} "后仰困难"]]]
-    [antd/form-item {:label "口腔"} [antd/input {:addonAfter "cm" :placeholder "张口"}]]
-    [antd/form-item {:label "Mallampati"}
-     [antd/radio-group {}
-      [antd/radio {:value "I"} "I"]
-      [antd/radio {:value "II"} "II"]
-      [antd/radio {:value "III"} "III"]
-      [antd/radio {:value "IV"} "IV"]]]
-    [antd/form-item {:label "颏颌距离"} [antd/input-number {:addonAfter "cm"}]]
-    [antd/form-item {:label "相关病史"}
-     [antd/checkbox-group {:style {:width "100%"}}
-      [antd/row
-       [antd/col {:span 8} [antd/checkbox {:value "facial_injury"} "颌面部损伤"]]
-       [antd/col {:span 8} [antd/checkbox {:value "tracheal_deviation"} "气管压迫移位"]]
-       [antd/col {:span 8} [antd/checkbox {:value "sleep_apnea"} "睡眠呼吸暂停综合征"]]
-       [antd/col {:span 8} [antd/checkbox {:value "acromegaly"} "肢端肥大"]]
-       [antd/col {:span 8} [antd/checkbox {:value "congenital_malformation"} "先天畸形"]]
-       [antd/col {:span 8} [antd/checkbox {:value "rheumatoid_arthritis"} "风湿性关节炎"]]
-       [antd/col {:span 24} [antd/checkbox {:value "other"} "其他"] [antd/input {:placeholder "请输入"}]]]]]
-    [antd/form-item {:label "胸"}
-     [antd/radio-group {}
-      [antd/radio {:value "normal"} "正常"]
-      [antd/radio {:value "barrel"} "桶状胸"]
-      [antd/radio {:value "pectus_excavatum"} "佝偻胸"]]]
-    ;; ... Add more physical examination fields as needed
-    ]])
+  [antd/form {:layout "vertical" :style {:padding-bottom "24px"}}
+   [antd/form-item {:label "一般状况"}
+    [antd/radio-group {}
+     [antd/radio {:value "bad"} "差"]
+     [antd/radio {:value "fair"} "尚可"]
+     [antd/radio {:value "average"} "一般"]
+     [antd/radio {:value "good"} "好"]]]
+   [antd/row {:gutter 16}
+    [antd/col {:span 8} [antd/form-item {:label "身高"} [antd/input-number {:addonAfter "cm"}]]]
+    [antd/col {:span 8} [antd/form-item {:label "体重"} [antd/input-number {:addonAfter "kg"}]]]
+    [antd/col {:span 8} [antd/form-item {:label "BP"} [antd/input {:addonAfter "mmHg"}]]]
+    [antd/col {:span 8} [antd/form-item {:label "PR"} [antd/input {:addonAfter "次/分"}]]]
+    [antd/col {:span 8} [antd/form-item {:label "RR"} [antd/input {:addonAfter "次/分"}]]]
+    [antd/col {:span 8} [antd/form-item {:label "T"} [antd/input {:addonAfter "°C"}]]]]
+   [antd/form-item {:label "精神行为"}
+    [antd/radio-group {}
+     [antd/radio {:value "normal"} "正常"]
+     [antd/radio {:value "drowsy"} "嗜睡"]
+     [antd/radio {:value "coma"} "昏迷"]
+     [antd/radio {:value "irritable"} "烦躁"]
+     [antd/radio {:value "delirium"} "谵妄"]
+     [antd/radio {:value "cognitive"} "认知障碍"]]]
+   [antd/form-item {:label "头颈部"}
+    [antd/checkbox-group {}
+     [antd/checkbox {:value "normal"} "无异常"]
+     [antd/checkbox {:value "scar"} "疤痕"]
+     [antd/checkbox {:value "short_neck"} "颈短"]
+     [antd/checkbox {:value "neck_mass"} "颈部肿块"]
+     [antd/checkbox {:value "limited_mobility"} "后仰困难"]]]
+   [antd/form-item {:label "口腔"} [antd/input {:addonAfter "cm" :placeholder "张口"}]]
+   [antd/form-item {:label "Mallampati"}
+    [antd/radio-group {}
+     [antd/radio {:value "I"} "I"]
+     [antd/radio {:value "II"} "II"]
+     [antd/radio {:value "III"} "III"]
+     [antd/radio {:value "IV"} "IV"]]]
+   [antd/form-item {:label "颏颌距离"} [antd/input-number {:addonAfter "cm"}]]
+   [antd/form-item {:label "相关病史"}
+    [antd/checkbox-group {:style {:width "100%"}}
+     [antd/row
+      [antd/col {:span 8} [antd/checkbox {:value "facial_injury"} "颌面部损伤"]]
+      [antd/col {:span 8} [antd/checkbox {:value "tracheal_deviation"} "气管压迫移位"]]
+      [antd/col {:span 8} [antd/checkbox {:value "sleep_apnea"} "睡眠呼吸暂停综合征"]]
+      [antd/col {:span 8} [antd/checkbox {:value "acromegaly"} "肢端肥大"]]
+      [antd/col {:span 8} [antd/checkbox {:value "congenital_malformation"} "先天畸形"]]
+      [antd/col {:span 8} [antd/checkbox {:value "rheumatoid_arthritis"} "风湿性关节炎"]]
+      [antd/col {:span 24} [antd/checkbox {:value "other"} "其他"] [antd/input {:placeholder "请输入"}]]]]]
+   [antd/form-item {:label "胸"}
+    [antd/radio-group {}
+     [antd/radio {:value "normal"} "正常"]
+     [antd/radio {:value "barrel"} "桶状胸"]
+     [antd/radio {:value "pectus_excavatum"} "佝偻胸"]]]
+   ;; ... Add more physical examination fields as needed
+   ])
 
 (defn lab-tests []
-  [antd/card {:title (r/as-element [antd/title {:level 5} "实验室检查"]) :variant "borderless"} ; Use wrapped components
-   [antd/form {:layout "vertical"}
-    [antd/title {:level 5} "血常规"]
-    [antd/row {:gutter 16}
-     [antd/col {:span 8} [antd/form-item {:label "RBC"} [antd/input {:addonAfter "x10¹²/L"}]]]
-     [antd/col {:span 8} [antd/form-item {:label "Hct"} [antd/input {:addonAfter "%"}]]]
-     [antd/col {:span 8} [antd/form-item {:label "PLT"} [antd/input {:addonAfter "x10⁹/L"}]]]
-     [antd/col {:span 8} [antd/form-item {:label "WBC"} [antd/input {:addonAfter "x10⁹/L"}]]]
-     [antd/col {:span 8}
-      [antd/form-item {:label "血型"}
-       [antd/radio-group {}
-        [antd/radio {:value "A"} "A"]
-        [antd/radio {:value "B"} "B"]
-        [antd/radio {:value "AB"} "AB"]
-        [antd/radio {:value "O"} "O"]]]]
-     [antd/col {:span 8}
-      [antd/form-item {:label "Rh"}
-       [antd/radio-group {}
-        [antd/radio {:value "negative"} "阴性"]
-        [antd/radio {:value "positive"} "阳性"]]]]]
-    [antd/form-item {:label "凝血检查"}
-     [antd/radio-group {}
-      [antd/radio {:value "normal"} "正常"]
-      [antd/radio {:value "abnormal"} "异常"]]]
-    [antd/form-item {:label "血糖值"} [antd/input {:addonAfter "mmol/L"}]]
-    ;; ... Add more lab test fields as needed
-    ]])
+  [antd/form {:layout "vertical" :style {:padding-bottom "24px"}}
+   [antd/title {:level 5} "血常规"]
+   [antd/row {:gutter 16}
+    [antd/col {:span 8} [antd/form-item {:label "RBC"} [antd/input {:addonAfter "x10¹²/L"}]]]
+    [antd/col {:span 8} [antd/form-item {:label "Hct"} [antd/input {:addonAfter "%"}]]]
+    [antd/col {:span 8} [antd/form-item {:label "PLT"} [antd/input {:addonAfter "x10⁹/L"}]]]
+    [antd/col {:span 8} [antd/form-item {:label "WBC"} [antd/input {:addonAfter "x10⁹/L"}]]]
+    [antd/col {:span 8}
+     [antd/form-item {:label "血型"}
+      [antd/radio-group {}
+       [antd/radio {:value "A"} "A"]
+       [antd/radio {:value "B"} "B"]
+       [antd/radio {:value "AB"} "AB"]
+       [antd/radio {:value "O"} "O"]]]]
+    [antd/col {:span 8}
+     [antd/form-item {:label "Rh"}
+      [antd/radio-group {}
+       [antd/radio {:value "negative"} "阴性"]
+       [antd/radio {:value "positive"} "阳性"]]]]]
+   [antd/form-item {:label "凝血检查"}
+    [antd/radio-group {}
+     [antd/radio {:value "normal"} "正常"]
+     [antd/radio {:value "abnormal"} "异常"]]]
+   [antd/form-item {:label "血糖值"} [antd/input {:addonAfter "mmol/L"}]]
+   ;; ... Add more lab test fields as needed
+   ])
+
+(defn assessment-result []
+  [antd/card {:variant "borderless" :style {:height "calc(100vh - 180px)"}}
+   [antd/tabs 
+    {:defaultActiveKey "1"
+     :type "card"
+     :size "large"
+     :tabPosition "left"
+     :style {:height "100%"}
+     :items [{:key "1"
+              :label "简要病史"
+              :children (r/as-element
+                         [:div {:style {:padding "0 16px" :height "100%" :overflowY "auto"}}
+                          [brief-medical-history]])}
+             {:key "2"
+              :label "体格检查"
+              :children (r/as-element
+                         [:div {:style {:padding "0 16px" :height "100%" :overflowY "auto"}}
+                          [physical-examination]])}
+             {:key "3"
+              :label "实验室检查"
+              :children (r/as-element
+                         [:div {:style {:padding "0 16px" :height "100%" :overflowY "auto"}}
+                          [lab-tests]])}]}]])
 
 (def menu-items
   [{:key "1" :icon (r/as-element [antd/laptop-outlined]) :label "麻醉管理"} ; Use wrapped icons
@@ -207,14 +211,11 @@
                           :label "门诊麻醉评估"
                           :children (r/as-element
                                      [antd/row {:gutter 16}
-                                      [antd/col {:span 8}
-                                       [patient-list]]
-                                      [antd/col {:span 16}
-                                       [:div {:style {:background "#fff" :padding 24 :height "calc(100vh - 150px)" :overflowY "auto"}}
-                                        [antd/title {:level 4} "评估结果"]
-                                        [brief-medical-history]
-                                        [physical-examination]
-                                        [lab-tests]]]])}
+                                      [antd/col {:span 6}
+                                       [antd/card {:title "患者列表" :variant "borderless" :style {:height "calc(100vh - 180px)"}}
+                                        [patient-list]]]
+                                      [antd/col {:span 18}
+                                       [assessment-result]]])}
                          {:key "2"
                           :label "门诊麻醉同意"
                           :children "门诊麻醉同意内容"}]}]]]])
