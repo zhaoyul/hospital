ef8b5140cc67111c78d812c27bea9a7d0a0457c0
kevin li <lizhoayu1@redcreation.cn>
2025-05-10 10:02:55 +0800

    [ref]: 提取 custom-sider-trigger 至新文件 comps.cljs
    - 将 `custom-sider-trigger` 函数从 `anesthesia_home.cljs` 移至新创建的 `comps.cljs` 文件.
    - 在 `anesthesia_home.cljs` 中更新对 `custom-sider-trigger` 的导入声明.
    - 将图标导入 (`LeftOutlined`, `RightOutlined`) 从 `anesthesia_home.cljs` 移至 `comps.cljs`.

--- src/cljs/hc/hospital/pages/anesthesia_home.cljs
+++ src/cljs/hc/hospital/pages/anesthesia_home.cljs
@@ -5,7 +5,7 @@
             [hc.hospital.subs :as subs]
             [hc.hospital.components.antd :as antd]
             [hc.hospital.components.form-components :as form-comp]
-            ["@ant-design/icons" :refer [LeftOutlined RightOutlined]]
+            [hc.hospital.pages.comps :refer [custom-sider-trigger]]
             ["antd" :refer [Form]]))
 
 (defn patient-list []
@@ -379,27 +379,6 @@
   [{:key "1" :icon (r/as-element [antd/laptop-outlined]) :label "麻醉管理"}
    {:key "2" :icon (r/as-element [antd/user-outlined]) :label "患者文书"}
    {:key "3" :icon (r/as-element [antd/notification-outlined]) :label "患者签到"}])
-
-(defn custom-sider-trigger [*collapsed? on-toggle]
-  [antd/button
-   {:type "primary"
-    :shape "circle"
-    :icon (if @*collapsed?
-            (r/as-element [:> RightOutlined])
-            (r/as-element [:> LeftOutlined]))
-    :on-click on-toggle
-    :style {:position "absolute"
-            :top "72px"      ;; 根据你的设计调整垂直位置
-            :right "-12px"   ;; 负值使其部分悬浮在Sider外部
-            :z-index 1       ;; 确保在Sider之上
-            :display "flex"
-            :align-items "center"
-            :justify-content "center"
-            :font-size "12px" ; 调整图标大小
-            :box-shadow "0 2px 8px rgba(0, 0, 0, 0.15)"
-            :background-color "#fff" ; 白色背景
-            :color "#1890ff"         ; Ant Design 主蓝色图标
-            :border "none"}}])
 
 (defn anesthesia-home-page []
   (let [active-tab @(rf/subscribe [::subs/active-tab])
---
+++ src/cljs/hc/hospital/pages/comps.cljs
@@ -0,0 +1,26 @@
+(ns hc.hospital.pages.comps
+  (:require
+   ["@ant-design/icons" :refer [LeftOutlined RightOutlined]]
+   [hc.hospital.components.antd :as antd]
+   [reagent.core :as r]))
+
+(defn custom-sider-trigger [*collapsed? on-toggle]
+  [antd/button
+   {:type "primary"
+    :shape "circle"
+    :icon (if @*collapsed?
+            (r/as-element [:> RightOutlined])
+            (r/as-element [:> LeftOutlined]))
+    :on-click on-toggle
+    :style {:position "absolute"
+            :top "72px"      ;; 根据你的设计调整垂直位置
+            :right "-12px"   ;; 负值使其部分悬浮在Sider外部
+            :z-index 1       ;; 确保在Sider之上
+            :display "flex"
+            :align-items "center"
+            :justify-content "center"
+            :font-size "12px" ; 调整图标大小
+            :box-shadow "0 2px 8px rgba(0, 0, 0, 0.15)"
+            :background-color "#fff" ; 白色背景
+            :color "#1890ff"         ; Ant Design 主蓝色图标
+            :border "none"}}])
