5e8e89447c5405b8cd5fac0a0a13cfeb46371a85
kevin li <12980829@qq.com>
2025-05-10 21:58:54 +0800

    [bug] 修复医生设置页面电子签名显示及上传问题
    - 修复: 完善医生列表电子签名的展示逻辑:
      - 当签名为 `nil` 时, 显示 "无签名".
      - 当签名为字符串 (URL) 时, 显示图片.
      - 当签名为包含 `:thumbUrl` 的对象时, 显示图片.
      - 其他无效情况统一显示 "无签名".
    - 修复: 纠正电子签名上传时 `FileReader` `onload` 回调中获取 `result` 的错误路径.
    - 优化: 移除 `antd` 中重复导入的 `Space` 组件.
    - 格式: 调整部分 UI 代码的格式与缩进.

--- src/cljs/hc/hospital/pages/settings.cljs
+++ src/cljs/hc/hospital/pages/settings.cljs
@@ -2,8 +2,7 @@
   (:require
    ["@ant-design/icons" :as icons]
    ["react" :as React]
-   ["antd" :refer [Button Form Input Modal Select Space Space Table Typography
-                   Upload]] ; Added Modal, Table, Space
+   ["antd" :refer [Button Form Input Modal Select Space Table Typography Upload]]
    [hc.hospital.events :as events]
    [hc.hospital.subs :as subs]
    [re-frame.core :as rf]
@@ -13,9 +12,10 @@
   (let [doctors @(rf/subscribe [::subs/doctors])
         modal-visible? @(rf/subscribe [::subs/doctor-modal-visible?])
         editing-doctor @(rf/subscribe [::subs/editing-doctor])
-        [form] (Form.useForm)] ; Ant Design Form instance
+        [form] (Form.useForm)]
 
     (React/useEffect (fn [] (rf/dispatch [::events/initialize-doctors])))
+
     [:div {:style {:flex 1
                    :background "#fff"
                    :borderRadius "4px"
@@ -26,7 +26,11 @@
                    :flexDirection "column"
                    :margin "16px"
                    :height "calc(100vh - 64px - 32px)"}}
-     [:> Typography.Title {:level 2 :style {:marginTop 0 :marginBottom "16px" :fontSize "18px" :fontWeight 500 :color "#333"}} "系统设置"]
+     [:> Typography.Title {:level 2
+                           :style {:marginTop 0 :marginBottom "16px"
+                                   :fontSize "18px" :fontWeight 500 :color "#333"}}
+      "系统设置"]
+
      [:div {:style {:marginBottom "16px"}}
       [:> Button {:type "primary"
                   :icon (r/as-element [:> icons/PlusOutlined])
@@ -34,34 +38,73 @@
                               (.resetFields form)
                               (rf/dispatch [::events/open-doctor-modal nil]))}
        "新增医生"]]
+
      [:> Table {:dataSource doctors
                 :rowKey :username
                 :style {:marginTop "16px"}
-                :columns  [{:title "姓名" :dataIndex "name" :key "name" :align "center"}
-                           {:title "账号" :dataIndex "username" :key "username" :align "center"}
-                           {:title "角色" :dataIndex "role" :key "role" :align "center"}
-                           {:title "电子签名" :dataIndex "signature" :key "signature" :align "center"
-                            :render (fn [signature _record]
-                                      (when signature
-                                        (if (or (string? signature) (:thumbUrl signature))
-                                          [:img {:src (if (string? signature) signature (:thumbUrl signature))
-                                                 :alt "签名"
-                                                 :style {:width "48px" :height "48px" :borderRadius "50%" :objectFit "cover" :border "1px solid #eee"}}]
-                                          [:span "无签名"])))}
-                           {:title "操作" :key "action" :align "center"
-                            :render (fn [_text record]
-                                      (let [doc (js->clj record :keywordize-keys true)]
+                :columns [{:title "姓名"
+                           :dataIndex "name"
+                           :key "name"
+                           :align "center"}
+
+                          {:title "账号"
+                           :dataIndex "username"
+                           :key "username"
+                           :align "center"}
+
+                          {:title "角色"
+                           :dataIndex "role"
+                           :key "role"
+                           :align "center"}
+
+                          {:title "电子签名"
+                           :dataIndex "signature"
+                           :key "signature"
+                           :align "center"
+                           :render (fn [signature _record]
+                                     (cond
+                                       (nil? signature) "无签名"
+                                       (string? signature)
+                                       (r/as-element [:img {:src signature
+                                                            :alt "签名"
+                                                            :style {:width "48px"
+                                                                    :height "48px"
+                                                                    :borderRadius "50%"
+                                                                    :objectFit "cover"
+                                                                    :border "1px solid #eee"}}])
+                                       (:thumbUrl signature)
+                                       (r/as-element [:img {:src (:thumbUrl signature)
+                                                            :alt "签名"
+                                                            :style {:width "48px"
+                                                                    :height "48px"
+                                                                    :borderRadius "50%"
+                                                                    :objectFit "cover"
+                                                                    :border "1px solid #eee"}}])
+                                       :else "无签名"))}
+
+                          {:title "操作"
+                           :key "action"
+                           :align "center"
+                           :render (fn [_text record]
+                                     (let [doc (js->clj record :keywordize-keys true)]
+                                       (r/as-element
                                         [:> Space {}
-                                         [:> Button {:type "primary" :ghost true :size "small" :icon (r/as-element [:> icons/EditOutlined])
+                                         [:> Button {:type "primary"
+                                                     :ghost true
+                                                     :size "small"
+                                                     :icon (r/as-element [:> icons/EditOutlined])
                                                      :on-click (fn []
                                                                  (.setFieldsValue form (clj->js {:name (:name doc)
                                                                                                  :username (:username doc)
                                                                                                  :role (:role doc)}))
                                                                  (rf/dispatch [::events/open-doctor-modal doc]))}
                                           "编辑"]
-                                         [:> Button {:danger true :ghost true :size "small" :icon (r/as-element [:> icons/DeleteOutlined])
+                                         [:> Button {:danger true
+                                                     :ghost true
+                                                     :size "small"
+                                                     :icon (r/as-element [:> icons/DeleteOutlined])
                                                      :on-click #(rf/dispatch [::events/delete-doctor (:username doc)])}
-                                          "删除"]]))}]}]
+                                          "删除"]])))}]}]
 
      (when modal-visible?
        [:> Modal {:title (if (:username editing-doctor) "编辑医生" "新增医生")
@@ -77,11 +120,19 @@
                   :initialValues (clj->js (select-keys editing-doctor [:name :username :role]))
                   :onFinish (fn [values]
                               (rf/dispatch [::events/save-doctor (js->clj values :keywordize-keys true)]))}
-         [:> Form.Item {:name "name" :label "姓名" :rules #js [{:required true :message "请输入姓名!"}]}
+         [:> Form.Item {:name "name"
+                        :label "姓名"
+                        :rules #js [{:required true :message "请输入姓名!"}]}
           [:> Input {}]]
-         [:> Form.Item {:name "username" :label "账号" :rules #js [{:required true :message "请输入账号!"}]}
+
+         [:> Form.Item {:name "username"
+                        :label "账号"
+                        :rules #js [{:required true :message "请输入账号!"}]}
           [:> Input {:disabled (boolean (:username editing-doctor))}]]
-         [:> Form.Item {:name "role" :label "角色" :rules #js [{:required true :message "请选择角色!"}]}
+
+         [:> Form.Item {:name "role"
+                        :label "角色"
+                        :rules #js [{:required true :message "请选择角色!"}]}
           [:> Select {:placeholder "选择角色"}
            [:> Select.Option {:value "麻醉医生"} "麻醉医生"]
            [:> Select.Option {:value "管理员"} "管理员"]
@@ -90,16 +141,21 @@
            [:> Select.Option {:value "统计"} "统计"]
            [:> Select.Option {:value "医务部统计"} "医务部统计"]
            [:> Select.Option {:value "护理管理员"} "护理管理员"]]]
-         [:> Form.Item {:name "signature-file" :label "电子签名" :valuePropName "fileList"}
+
+         [:> Form.Item {:name "signature-file"
+                        :label "电子签名"
+                        :valuePropName "fileList"}
           [:> Upload {:name "signature"
                       :listType "picture-card"
                       :showUploadList false
                       :beforeUpload (fn [file]
                                       (let [reader (js/FileReader.)]
                                         (set! (.-onload reader)
-                                              #(rf/dispatch [::events/update-editing-doctor-field :signature (.. % -result -target)]))
+                                              #(rf/dispatch [::events/update-editing-doctor-field :signature (.. % -target -result)]))
                                         (.readAsDataURL reader file))
                                       false)}
            (if (and editing-doctor (:signature editing-doctor) (not (object? (:signature editing-doctor))))
              [:img {:src (:signature editing-doctor) :alt "签名" :style {:width "100%"}}]
-             [:div [:> icons/PlusOutlined] [:div {:style {:marginTop 8}} "上传"]])]]]])]))
+             [:div
+              [:> icons/PlusOutlined]
+              [:div {:style {:marginTop 8}} "上传"]])]]]])]))
