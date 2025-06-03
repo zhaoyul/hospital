0d05869c3034a0eefc2190fbc96ef5aca30f4123
kevin li <12980829@qq.com>
2025-06-03 21:42:15 +0800

    [ref] 优化 custom-styled-card 组件
    - 移除标题中不必要的 on-click 事件.
    - 移除未使用的 `@ant-design/icons` 导入.

--- src/cljs/hc/hospital/ui_helpers.cljs
+++ src/cljs/hc/hospital/ui_helpers.cljs
@@ -1,14 +1,11 @@
 (ns hc.hospital.ui-helpers
   (:require ["antd" :refer [Card]]
-            ["@ant-design/icons" :as icons]
             [reagent.core :as r]))
 
 (defn custom-styled-card 
   "创建统一样式的卡片，支持双击事件和自定义样式"
   [icon title-text header-bg-color content & {:keys [on-click card-style card-body-style view-state]}]
-  [:> Card {:title (r/as-element [:span {:on-click #(js/alert "haha")
-                                         }
-                                  icon title-text])
+  [:> Card {:title (r/as-element [:span icon title-text])
             :styles {:header {:background header-bg-color}
                      :body (merge {:background "#ffffff"} card-body-style)}
             :type "inner"
