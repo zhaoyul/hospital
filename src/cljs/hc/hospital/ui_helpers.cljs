f89455c2033f03275b470c7b6a2d18f8337f02f3
kevin li <12980829@qq.com>
2025-06-10 21:43:50 +0800

    [ref] 调整卡片标题的点击事件处理机制
    - 将 `on-click` 事件处理器直接绑定到卡片标题的 `div` 元素.
    - 移除原 `Card` 组件上依赖 `view-state` 的条件性 `onClick` 处理器.
    - 实现卡片标题的 `on-click` 事件无条件触发 (当 `on-click` 回调提供时).[]

--- src/cljs/hc/hospital/ui_helpers.cljs
+++ src/cljs/hc/hospital/ui_helpers.cljs
@@ -5,11 +5,9 @@
 (defn custom-styled-card 
   "创建统一样式的卡片，支持双击事件和自定义样式"
   [icon title-text header-bg-color content & {:keys [on-click card-style card-body-style view-state]}]
-  [:> Card {:title (r/as-element [:span icon title-text])
+  [:> Card {:title (r/as-element [:div {:on-click on-click} [:span icon title-text]])
             :styles {:header {:background header-bg-color}
                      :body (merge {:background "#ffffff"} card-body-style)}
             :type "inner"
-            :style (merge {:marginBottom "12px"} card-style)
-            :onClick (when (= :summary view-state)
-                       on-click)}
+            :style (merge {:marginBottom "12px"} card-style)}
    content])
