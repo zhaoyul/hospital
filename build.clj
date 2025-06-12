dd6939787c3bd3ed29059d8ba3fb3695e9ec3a06
KevinLi <li_zhaoyu@msn.com>
2025-06-12 14:43:25 +0800

    Include prod paths in build basis

--- build.clj
+++ build.clj
@@ -16,7 +16,12 @@
 (def target-dir "target")
 (def class-dir (str target-dir "/" "classes"))
 (def uber-file (format "%s/%s-standalone.jar" target-dir (name lib)))
-(def basis (b/create-basis {:project "deps.edn"}))
+(def basis
+  ;; Include production source and resources so that namespaces such as
+  ;; `hc.hospital.env` resolve during compilation.  These paths are not part of
+  ;; `deps.edn` by default, so we add them via `:extra` to the basis.
+  (b/create-basis {:project "deps.edn"
+                   :extra {:paths ["env/prod/clj" "env/prod/resources"]}}))
 
 (defn write-version-file []
   (println "Writing version.properties file...")
