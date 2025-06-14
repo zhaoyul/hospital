d6ce9463b10486b0016acd93a1d9469cc248cf8a
KevinLi <li_zhaoyu@msn.com>
2025-06-14 15:45:25 +0800

    修复评估页面无法滚动导致按钮消失的问题

--- src/cljs/hc/hospital/pages/anesthesia_home.cljs
+++ src/cljs/hc/hospital/pages/anesthesia_home.cljs
@@ -87,9 +87,9 @@
 
 (defn right-side "患者麻醉管理\"patients\", 问卷列表\"assessment\", 系统设置\"settings\"" [active-tab]
 
-  (let [role (:role @(rf/subscribe [::subs/current-doctor]))]
+   (let [role (:role @(rf/subscribe [::subs/current-doctor]))]
     [:> Layout {:style {:height "calc(100vh - 64px)"
-                        :overflow "hidden"
+                        :overflow "auto"
                         :background "#f0f2f5"}}
      [:div {:style {:padding "24px"}}
       (case active-tab
