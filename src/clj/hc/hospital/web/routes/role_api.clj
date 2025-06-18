a7794aaf6c5f2a3a57a98c67da0934d855f508d8
KevinLi <li_zhaoyu@msn.com>
2025-06-18 20:12:58 +0800

    fix role api route param passing

--- src/clj/hc/hospital/web/routes/role_api.clj
+++ src/clj/hc/hospital/web/routes/role_api.clj
@@ -21,44 +21,45 @@
                 exception/wrap-exception]})
 
 (defn role-api-routes [opts]
-  [["/roles"
-    {:get {:summary "获取角色列表"
-           :handler (fn [req]
-                      (role-api/list-roles (assoc req :integrant-deps opts)))
-           :middleware [wrap-restricted]}
-     :post {:summary "创建角色"
-            :parameters {:body {:name string?}}
-            :handler (fn [req]
-                       (role-api/create-role! {:integrant-deps opts
-                                               :body-params (:body-params req)}))
-            :middleware [wrap-restricted]}}]
-   ["/roles/:id/permissions"
-    {:get {:summary "获取角色权限"
-           :parameters {:path {:id int?}}
-           :handler (fn [req]
-                      (role-api/get-role-permissions {:integrant-deps opts
-                                                       :path-params (:path-params req)}))
-           :middleware [wrap-restricted]}
-     :put {:summary "更新角色权限"
-           :parameters {:path {:id int?}
-                        :body {:permission_ids [int?]}}
-           :handler (fn [req]
-                      (role-api/update-role-permissions! {:integrant-deps opts
-                                                           :path-params (:path-params req)
-                                                           :body-params (:body-params req)}))
-           :middleware [wrap-restricted]}}]
-   ["/roles/:id"
-    {:delete {:summary "删除角色"
-              :parameters {:path {:id int?}}
+  (let [query-fn (:query-fn opts)]
+    [["/roles"
+      {:get {:summary "获取角色列表"
+             :handler (fn [req]
+                        (role-api/list-roles (assoc req :query-fn query-fn)))
+             :middleware [wrap-restricted]}
+       :post {:summary "创建角色"
+              :parameters {:body {:name string?}}
               :handler (fn [req]
-                         (role-api/delete-role! {:integrant-deps opts
-                                                  :path-params (:path-params req)}))
+                         (role-api/create-role! {:query-fn query-fn
+                                                 :body-params (:body-params req)}))
               :middleware [wrap-restricted]}}]
-   ["/permissions"
-    {:get {:summary "获取权限列表"
-           :handler (fn [req]
-                      (role-api/list-permissions (assoc req :integrant-deps opts)))
-           :middleware [wrap-restricted]}}]])
+     ["/roles/:id/permissions"
+      {:get {:summary "获取角色权限"
+             :parameters {:path {:id int?}}
+             :handler (fn [req]
+                        (role-api/get-role-permissions {:query-fn query-fn
+                                                         :path-params (:path-params req)}))
+             :middleware [wrap-restricted]}
+       :put {:summary "更新角色权限"
+             :parameters {:path {:id int?}
+                          :body {:permission_ids [int?]}}
+             :handler (fn [req]
+                        (role-api/update-role-permissions! {:query-fn query-fn
+                                                             :path-params (:path-params req)
+                                                             :body-params (:body-params req)}))
+             :middleware [wrap-restricted]}}]
+     ["/roles/:id"
+      {:delete {:summary "删除角色"
+                :parameters {:path {:id int?}}
+                :handler (fn [req]
+                           (role-api/delete-role! {:query-fn query-fn
+                                                    :path-params (:path-params req)}))
+                :middleware [wrap-restricted]}}]
+     ["/permissions"
+      {:get {:summary "获取权限列表"
+             :handler (fn [req]
+                        (role-api/list-permissions (assoc req :query-fn query-fn)))
+             :middleware [wrap-restricted]}}]]))
 
 (derive :reitit.routes/role-api :reitit/routes)
 
